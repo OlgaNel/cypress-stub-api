8069f139e8ae01bfebcc1b5e6446b0642f5c9229
Olga Nelipa <olga.nelipa@onereach.com>
2022-07-21 11:45:12 +0200

    test to check rebase

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -6,8 +6,12 @@ describe('Feedback Form Tests', () => {
     it('should send API request and display Error component', () => {
         cy.visit(Cypress.env('HOST'));
         cy.get(submitBtn).should('be.visible');
-        cy.get(submitBtn).click();
+        //cy.get(submitBtn).click();
     });
+
+    it('test', () => {
+        //asdasdsadadasdadasdasd
+    })
 
     // TO DO: test the sucess path of this application by using cy.intercept! 
     // You don't need to mock or touch the code!
